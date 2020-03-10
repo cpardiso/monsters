e60d692f6b994bc563c3cfe9bd1eedc5e28685a4
dano <cpardiso@gmail.com>
2020-03-10 21:11:01 +0330

    Updates

---
+++ precache-manifest.4adc8ac1b051dcb0bac299eef5faedde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da625e82bffbbdb7b9fe6682a4222046",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "d031f67f11ddb41e1d41",
+    "url": "/monsters/static/css/main.305ecdd1.chunk.css"
+  },
+  {
+    "revision": "2b7d9edd948003b96165",
+    "url": "/monsters/static/js/2.b6796cae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.b6796cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d031f67f11ddb41e1d41",
+    "url": "/monsters/static/js/main.b05b8d0e.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
